5cfb4c48ba7a097bc7dd527a056d14243a23d8fd
ajay09 <ajay199109@gmail.com>
2024-04-06 23:01:51 +0100

    Create initial migrations.

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "server",
+    "account",
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTH_USER_MODEL = "account.Account"
