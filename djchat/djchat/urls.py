fafbd804bf6e6322501cad7bb9240aa995873206
ajay09 <ajay199109@gmail.com>
2024-04-07 23:45:09 +0100

    Configure media url.

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
@@ -39,3 +41,6 @@ urlpatterns += [
 ]
 
 urlpatterns += router.urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
