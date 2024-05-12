0cc6d0a4141cb86f52b742e66809a19b3b5bc09a
ajay09 <ajay199109@gmail.com>
2024-05-12 13:48:34 +0100

    Scroll - scroll to bottom.

--- reactchat/src/components/Main/Scroll.tsx
+++ reactchat/src/components/Main/Scroll.tsx
@@ -29,41 +29,6 @@ const ScrollContainer = styled(Box)(() => ({
 }));
 
 const Scroll = ({ children }: ScrollProps) => {
-return <ScrollContainer>{children}</ScrollContainer>;
-};
-
-export default Scroll;
-
-
-
-/*
-interface ScrollProps {
-  children: React.ReactNode;
-}
-
-const ScrollContainer = styled(Box)(() => ({
-  height: `calc(100vh - 190px)`,
-  overflowY: "scroll",
-  "&::-webkit-scrollbar": {
-    width: "8px",
-    height: "8px",
-  },
-  "&::-webkit-scrollbar-thumb": {
-    backgroundColor: "#888",
-    borderRadius: "4px",
-  },
-  "&::-webkit-scrollbar-thumb:hover": {
-    backgroundColor: "#555",
-  },
-  "&::-webkit-scrollbar-track": {
-    // backgroundColor: "#f0f0f0",
-  },
-  "&::-webkit-scrollbar-corner": {
-    backgroundColor: "transparent",
-  },
-}));
-
-const Scroll = ({ children }: ScrollProps) => {
   const scrollRef = useRef<HTMLDivElement>(null);
 
   const scrollToBottom = useCallback(() => {
@@ -78,5 +43,5 @@ const Scroll = ({ children }: ScrollProps) => {
 
   return <ScrollContainer ref={scrollRef}>{children}</ScrollContainer>;
 };
+
 export default Scroll;
-*/
